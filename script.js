4efe5803639cf33bd6d36a3997411d872c1804d3
891776-afk <891776@pdsb.net>
2026-08-25 17:05:03 -0400

    Buttons

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ const noteButton =
 document.getElementById("noteButton");
 const notePopup =
 document.getElementById("notePopup");
+const backButton =
+document.getElementById("closeButton");
 
 noteButton.addEventListener("click",
     function() {
         notePopup.style.display = "flex";
+    }
+);
+
+backButton.addEventListener("click",
+    function() {
+        notePopup.style.display = "none";
     }
 );
