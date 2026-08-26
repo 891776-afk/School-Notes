f28bc1608363d3ca095cde20f8f9d26fc39d31c8
891776-afk <891776@pdsb.net>
2026-08-26 13:38:21 -0400

    notes Update

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const noteButton =
 document.getElementById("noteButton");
+
 const notePopup =
 document.getElementById("notePopup");
+
 const backButton =
 document.getElementById("closeButton");
+
+const saveButton =
+document.getElementById("saveButton");
+
+const noteTitle =
+document.getElementById("noteTitle");
+
+const noteDescription =
+document.getElementById("noteDescription");
+
+const noteNotes =
+document.getElementById("noteNotes");
+
+const notesGrid =
+document.getElementById("notesGrid");
+
 
 noteButton.addEventListener("click",
     function() {
@@ -13,6 +31,30 @@ noteButton.addEventListener("click",
 
 backButton.addEventListener("click",
     function() {
+        notePopup.style.display = "none";
+    }
+);
+
+saveButton.addEventListener("click",
+    function() {
+
+        const title = noteTitle.ariaValueMax;
+        const description =
+    noteDescription.Value;
+        const notes = noteNotes.Value;
+
+        const noteCard =
+    document.createElement("div");
+        
+        noteCard.classList.add("note-card");
+
+        noteCard.innerHTML = `
+            <h3>${Title}</h3>
+            <p>${description}</p>
+            <p>${notes}</p>
+        `;
+
+        notesGrid.appendChild(noteCard);
         notePopup.style.display = "none";
     }
 );
